03e099747dda72d92da7f9f1b1a705f1c661b553
Ku-Andrii <andriy.kutsenko.ph@gmail.com>
2021-02-20 02:27:02 +0200

    Create test

--- test_list.py
+++ test_list.py
@@ -14,6 +14,10 @@ class TestListMethods(unittest.TestCase):
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
 
+    def test_andrii_kutsenko_fi_94(self):
+        list1 = [4, 5, 7, 3, 8, 6, 3, 9]
+        self.assertEqual(delete_element(list1), [4, 5, 7, 3, 6, 3, 9])
+
 
 if __name__ == '__main__':
     unittest.main()
